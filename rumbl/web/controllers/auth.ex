75f151bd3b8db598650c66ac703fa844da0c2a2a
Arafat M <github@arafatm.com>
2017-06-14 12:42:41 -0700

    Rumbl.Auth.login method to auto-login when a new user is created

--- rumbl/web/controllers/auth.ex
+++ rumbl/web/controllers/auth.ex
@@ -10,4 +10,11 @@ defmodule Rumbl.Auth do
     user    = user_id && repo.get(Rumbl.User, user_id) # lookup user if user_id exists
     assign(conn, :current_user, user)
   end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true) # resets session to protect from session fixation attack
+  end
 end
