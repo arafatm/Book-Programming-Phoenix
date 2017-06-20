9697acea53c9f5444fe093473e69204893011912
Arafat M <github@arafatm.com>
2017-06-19 19:05:16 -0700

    AuthTest login puts the user in the session

--- rumbl/test/controllers/auth_test.exs
+++ rumbl/test/controllers/auth_test.exs
@@ -11,17 +11,27 @@ defmodule Rumbl.AuthTest do
     {:ok, %{conn: conn}}
   end
 
-  test 'authenticate_user halts when no current_user exists', %{conn: conn} do
+  test "authenticate_user halts when no current_user exists", %{conn: conn} do
     conn = Auth.authenticate_user(conn, [])
     assert conn.halted
   end
 
-  test 'authenticate_user continues when the current_user exists', %{conn: conn} do
+  test "authenticate_user continues when the current_user exists", %{conn: conn} do
     conn =
       conn
       |> assign(:current_user, %Rumbl.User{})
       |> Auth.authenticate_user([])
 
     refute conn.halted
+  end
+
+  test "login puts the user in the session", %{conn: conn} do
+    login_conn =
+      conn
+      |> Auth.login(%Rumbl.User{id: 123})
+      |> send_resp(:ok, "")
+
+    next_conn = get(login_conn, "/")
+    assert get_session(next_conn, :user_id) == 123
   end
 end
