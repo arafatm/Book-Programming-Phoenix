ede3da347c384e3c1bf3285164915829b3f572f4
Arafat M <github@arafatm.com>
2017-06-23 11:06:00 -0700

    Rewrite Counter using OTP GenServer

--- rumbl/lib/rumbl/counter.ex
+++ rumbl/lib/rumbl/counter.ex
@@ -1,40 +1,46 @@
+
 defmodule Rumbl.Counter do
+  use GenServer
 
-  # Client Code
+  ########
+  # Client
+  ########
 
-  def inc(pid), do: send(pid, :inc) # async send :inc to server proc
+  # GenServer.cast sends async message
+  def inc(pid), do: GenServer.cast(pid, :inc)
 
-  def dec(pid), do: send(pid, :dec)
+  def dec(pid), do: GenServer.cast(pid, :dec)
 
-  def val(pid, timeout \\ 5000) do
-    ref = make_ref()
-    send(pid, {:val, self(), ref})
-    receive do                      # wait for response from server
-      {^ref, val} -> val    # only match tuple with exact ^ref
-    after timeout -> exit(:timeout) # exit if no resp w/in timeout
-    end
+  # GenServer.call sends synch message
+  def val(pid) do
+    GenServer.call(pid, :val)
   end
 
-  # Server Code
+  ########
+  # Server
+  ########
 
-
-  # OTP requires start_link
-  def start_link(initial_val) do 
-    {:ok, spawn_link(fn -> listen(initial_val) end)} # spawn a proc and return {:ok, pid}
+  # Start a GenServer: spawns new proc and invokes init
+  def start_link(initial_val) do
+    GenServer.start_link(__MODULE__, initial_val)
   end
 
-  defp listen(val) do
-    receive do
-      # listen for def inc(pid) or dec(pid) from above
-      :inc -> listen(val + 1) 
-      :dec -> listen(val - 1)
+  def init(initial_val) do
+    {:ok, initial_val}
+  end
 
-      {:val, sender, ref} ->
-        IO.puts ":val"
-        IO.puts "- sender:  #{inspect sender}"
-        IO.puts "- ref:  #{inspect ref}"
-        send sender, {ref, val}
-        listen(val) # use recursion to keep server alive
-    end
+  # async functions. Note `:noreply`
+  def handle_cast(:inc, val) do
+    {:noreply, val + 1}
+  end
+
+  # async functions. Note `:noreply`
+  def handle_cast(:dec, val) do
+    {:noreply, val - 1}
+  end
+
+  # synchronous function
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
   end
 end
