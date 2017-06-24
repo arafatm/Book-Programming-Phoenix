759de041377fdbad60ea2783d4268181ca130729
Arafat M <github@arafatm.com>
2017-06-24 11:20:50 -0700

    InfoSys add process monitor

--- rumbl/lib/rumbl/info_sys.ex
+++ rumbl/lib/rumbl/info_sys.ex
@@ -22,12 +22,39 @@ defmodule Rumbl.InfoSys do
 
     backends
     |> Enum.map(&spawn_query(&1, query, limit))
+    |> await_results(opts)                # Wait for results
+    |> Enum.sort(&(&1.score >= &2.score)) # sort by score
+    |> Enum.take(limit)                   # only report top results
   end
 
   defp spawn_query(backend, query, limit) do 
     query_ref = make_ref()
     opts = [backend, query, query_ref, self(), limit]
     {:ok, pid} = Supervisor.start_child(Rumbl.InfoSys.Supervisor, opts)
-    {pid, query_ref}
+    monitor_ref = Process.monitor(pid)      # monitor the child process
+    {pid, monitor_ref, query_ref}
+  end
+
+  defp await_results(children, _opts) do
+    await_result(children, [], :infinity)
+  end
+
+  # Accumulate results
+  defp await_result([head|tail], acc, timeout) do
+    {pid, monitor_ref, query_ref} = head
+
+    receive do
+      {:results, ^query_ref, results} -> 
+        # :flush removes :DOWN message in case it's delivered before we drop the monitor
+        Process.demonitor(monitor_ref, [:flush])
+        await_result(tail, results ++ acc, timeout)
+      {:DOWN, ^monitor_ref, :process, ^pid, _reason} -> 
+        await_result(tail, acc, timeout)
+    end
+  end
+
+  # Return accumulated results from previous await_results
+  defp await_result([], acc, _) do 
+    acc
   end
 end
