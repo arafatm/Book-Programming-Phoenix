83a866b34c6c65e793bae4b32d25bda0de70fba7
Arafat M <github@arafatm.com>
2017-07-09 14:40:31 -0700

    typo

--- rumbl/web/channels/video_channel.ex
+++ rumbl/web/channels/video_channel.ex
@@ -46,10 +46,10 @@ defmodule Rumbl.VideoChannel do
 
   defp broadcast_annotation(socket, annotation) do  
     annotation = Repo.preload(annotation, :user)
-    rendered_ann = Phoenix.View.render(AnnotationView, 'annotation.json', %{
+    rendered_ann = Phoenix.View.render(AnnotationView, "annotation.json", %{
       annotation: annotation
     })
-    broadcast! socket, 'new_annotation', rendered_ann
+    broadcast! socket, "new_annotation", rendered_ann
   end
 
   defp compute_additional_info(ann, socket) do
